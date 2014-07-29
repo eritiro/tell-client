fa1a60c847dd9ac3c2214dd0bee8bad6dd419aa8
eritiro <eritiro@gmail.com>
2014-07-29 20:14:42 -0300

    cordova mock initializes document

--- www/js/cordovamock.js
+++ www/js/cordovamock.js
@@ -1,9 +1,18 @@
 var cordova = cordova || {
-	plugins: { 
+	plugins: {
 		barcodeScanner: {
 			scan: function (success, error) {
 				success({ text: 'urlafip' });
 			}
 		}
 	}
+}
+
+
+document.onreadystatechange = function (){
+  var event;
+  event = document.createEvent("HTMLEvents");
+  event.initEvent("deviceready", true, true);
+  event.eventName = "deviceready";
+  document.dispatchEvent(event);
 }
