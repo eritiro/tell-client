ec419a91fbd6c63e747e460c400f62afd79d33f8
Diego Sisto <suelopoder@gmail.com>
2014-07-29 23:18:29 -0300

    Se unió el login

--- www/js/controllers/main.js
+++ www/js/controllers/main.js
@@ -1,12 +1,22 @@
 'use strict';
 
+var POSTA = {
+  serverUrl: 'http://tell.startmeapps.com/users/sign_in.json'
+};
+
+var POMELO = {
+  serverUrl: 'http://192.168.0.6/users/sign_in.json'
+};
+
+var config = POSTA;
+
 // Declare app level module which depends on filters, and services
 angular.module('tell', [
   'ngRoute',
   'tell.controllers'
 ]).
 config(['$routeProvider', function($routeProvider) {
-$routeProvider.when('/', {templateUrl: 'partials/home.html', controller: 'Home'});
+  $routeProvider.when('/', {templateUrl: 'partials/home.html', controller: 'Home'});
   $routeProvider.when('/scan', {templateUrl: 'partials/scan.html', controller: 'Scan'});
   $routeProvider.otherwise({redirectTo: '/'});
 }]);
@@ -14,31 +24,37 @@ $routeProvider.when('/', {templateUrl: 'partials/home.html', controller: 'Home'}
 /* Controllers */
 angular.module('tell.controllers', ['Devise'])
   .config(function(AuthProvider) {
-    AuthProvider.loginPath('http://tell.startmeapps.com/users/sign_in.json');
+    AuthProvider.loginPath(config.serverUrl);
   })
-  .controller('Home', function(Auth) {
-    var credentials = {
-        email: 'test@test.com',
-        password: 'test'
-    };
-
-    Auth.login(credentials).then(function(user) {
-        document.getElementById("console").innerHTML = 'user ' + user;
-    }, function(error) {
-        document.getElementById("console").innerHTML = 'error ' + error;
-        console.log(error); // => {id: 1, ect: '...'}
-    });
+  .controller('Home', function(Auth, $scope) {
+    $scope.userEmail = 'test@test.com';
+    $scope.userPass = 'test';
+    
+    $scope.submit = function() {
+      var credentials = {
+        email: $scope.userEmail,
+        password: $scope.userPass
+      };
+      
+      Auth.login(credentials).then(function(user) {
+        console.log("Todo ok");
+      }, function(error) {
+        console.log("Caca");
+      });
+    }
+  })
+  .controller('Login', function(Auth, $scope) {
   })
   .controller('Scan', ['$scope', function($scope) {
   	// TODO mover a algún lado (servicios?)
-	cordova.plugins.barcodeScanner.scan(
-		function (result) {
-			$scope.result = result.text;
-		}, 
-		function (error) {
-			$scope.result = "Falló el scan: " + error;
-		}
-	);
+	  cordova.plugins.barcodeScanner.scan(
+		  function (result) {
+			  $scope.result = result.text;
+		  }, 
+		  function (error) {
+			  $scope.result = "Falló el scan: " + error;
+		  }
+	  );
   }]);
 
 document.addEventListener("deviceready", function() {
