d6b6941022d1445dcc39cb3a5bcb94340714e77d
Diego Sisto <suelopoder@gmail.com>
2014-07-29 22:42:40 -0300

    Agregué módulo devise para autenticación

--- www/js/controllers/main.js
+++ www/js/controllers/main.js
@@ -6,16 +6,29 @@ angular.module('tell', [
   'tell.controllers'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/', {templateUrl: 'partials/home.html', controller: 'Home'});
+$routeProvider.when('/', {templateUrl: 'partials/home.html', controller: 'Home'});
   $routeProvider.when('/scan', {templateUrl: 'partials/scan.html', controller: 'Scan'});
   $routeProvider.otherwise({redirectTo: '/'});
 }]);
 
 /* Controllers */
-angular.module('tell.controllers', [])
-  .controller('Home', ['$scope', function($scope) {
+angular.module('tell.controllers', ['Devise'])
+  .config(function(AuthProvider) {
+    AuthProvider.loginPath('http://tell.startmeapps.com/users/sign_in.json');
+  })
+  .controller('Home', function(Auth) {
+    var credentials = {
+        email: 'test@test.com',
+        password: 'test'
+    };
 
-  }])
+    Auth.login(credentials).then(function(user) {
+        document.getElementById("console").innerHTML = 'user ' + user;
+    }, function(error) {
+        document.getElementById("console").innerHTML = 'error ' + error;
+        console.log(error); // => {id: 1, ect: '...'}
+    });
+  })
   .controller('Scan', ['$scope', function($scope) {
   	// TODO mover a algún lado (servicios?)
 	cordova.plugins.barcodeScanner.scan(
