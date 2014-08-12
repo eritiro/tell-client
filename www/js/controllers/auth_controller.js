85bf21fae1b8f75b0cd0fccba921eadb88fa0755
Diego Sisto <suelopoder@gmail.com>
2014-08-12 20:01:32 -0300

    Autologin cuando ya te logeaste alguna vez

--- www/js/controllers/auth_controller.js
+++ www/js/controllers/auth_controller.js
@@ -2,6 +2,28 @@
 
 angular.module('tell.controllers')
   .controller('AuthController', function(Auth, $scope, $location, userStorageService) {
+  
+    var tryLogin = function(credentials) {
+      Auth.login(credentials).then(function(user) {
+        user.password = credentials.password;
+        
+        userStorageService.storeData(user);
+        $location.path("/home");
+      }, function(error) {
+        // TODO ver qué hacer con error de auth
+      });
+    }
+  
+    var userData = userStorageService.getData();
+    if (userData) {
+      var credentials = {
+        email: userData.email,
+        password: userData.password
+      };
+      
+      tryLogin(credentials);
+    }
+    
     $scope.userEmail = 'test@test.com';
     $scope.userPass = 'test';
     $scope.error = false;
@@ -11,15 +33,8 @@ angular.module('tell.controllers')
         email: $scope.userEmail,
         password: $scope.userPass
       };
-
-      Auth.login(credentials).then(function(user) {
-        userStorageService.storeData(user);
-        $location.path("/home");
-      }, function(error) {
-        // TODO ver qué hacer con error de auth
-        $scope.error = true;
-        $scope.userEmail = 'HOLIS';
-      });
-    }
+      
+      tryLogin(credentials);
+    };
   });
 
