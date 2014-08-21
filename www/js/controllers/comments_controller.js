73737b09ec455c7447488839aee9bac31f74276a
Diego Sisto <suelopoder@gmail.com>
2014-08-21 20:05:16 -0300

    Agregué el puntaje para editar antes de postear el comentario

--- www/js/controllers/comments_controller.js
+++ www/js/controllers/comments_controller.js
@@ -1,17 +1,27 @@
 'use strict';
 
 angular.module('tell.controllers')
-  .controller('CommentsController', function($scope, $routeParams, Comment, $resource) {
+  .controller('CommentsController', function($scope, $routeParams, Comment, $window) {
     var id = $routeParams.locationId;
     var score = $routeParams.score;
     
     $scope.score = score;
-    $scope.text = 'Pomelo es JIPI. Pomelo es puto. SE ESCRIBE JIPI.';
+    $scope.text = '';
+    
+    $scope.setScore = function(index) {
+      $scope.score = index+1;
+    }
     
     $scope.send = function() {
+      if (!$scope.text) {
+        // TODO decidir si se puede o no postear sin mensaje
+        return;
+      }
+      
       var comment = new Comment({ locationId: id });
       comment['text'] = $scope.text;
       comment['score'] = $scope.score;
       comment.$save();
+      $window.history.back();
     }
   });
