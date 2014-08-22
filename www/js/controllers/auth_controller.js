aa3da44cf4440619d68e93569fe8e6556cc0a221
Diego Sisto <suelopoder@gmail.com>
2014-08-21 21:07:58 -0300

    Avances con la llamada de login a FB

--- www/js/controllers/auth_controller.js
+++ www/js/controllers/auth_controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('tell.controllers')
-  .controller('AuthController', function(Auth, $scope, $location, userSession, facebookService, $http) {
+  .controller('AuthController', function(Auth, $scope, $location, userSession, facebookService, $rootScope) {
 
     var tryLogin = function(credentials) {
       $scope.ready = false;
@@ -24,7 +24,36 @@ angular.module('tell.controllers')
     };
 
     $scope.fbLogin = function() {
-      facebookService.login();
+      facebookService.login(
+        function(fbUserData) { // FB login ok
+          document.querySelector("#debug").innerHTML = "Login con FB!!! " + JSON.stringify(fbUserData);
+          
+          // TODO pegarle al server y obtener user
+          var user = { 
+             "id":3,
+             "username":"test",
+             "email":"test@test.com",
+             "created_at":"2014-07-29T23:05:11.000Z",
+             "updated_at":"2014-08-20T01:01:19.717Z",
+             "picture_file_name":null,
+             "picture_content_type":null,
+             "picture_file_size":null,
+             "picture_updated_at":null,
+             "authentication_token":"28x9DULhkWn4Nsw-v_d2"
+          };
+          
+          userSession.storeUser(user);
+          $location.path("/home");
+          
+          // Hack to make angular work with corodva barcode plugin
+          if (!$rootScope.$$phase) {
+            $scope.$apply();
+          }
+        },
+        function(errorData) { // FB login error
+          document.querySelector("#debug").innerHTML = "Falló login FB " + JSON.stringify(errorData);
+        }
+      );
     };
 
     $scope.ready = true;
