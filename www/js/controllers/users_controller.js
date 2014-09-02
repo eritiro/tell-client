38d42b6efdfe1bf9533aa5765359e648bef4d0bd
Diego Sisto <suelopoder@gmail.com>
2014-09-01 22:11:55 -0300

    Desde el users controller el back button sale de la app

--- www/js/controllers/users_controller.js
+++ www/js/controllers/users_controller.js
@@ -3,6 +3,10 @@
 angular.module('tell.controllers')
   .controller('UsersController', function(Auth, $scope, $location, userSession, formHelper, facebookService, $rootScope) {
 
+    $scope.$on("back_button", function() {
+      navigator.app.exitApp();
+    });
+    
     $scope.user = formHelper.model;
 
     function nextStep(user){
