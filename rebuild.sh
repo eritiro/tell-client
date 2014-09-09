8c665b43210d4bb3f2cf97b885e452c5b430348a
eritiro <eritiro@gmail.com>
2014-09-09 01:37:52 -0300

    reverting change

--- rebuild.sh
+++ rebuild.sh
@@ -1,8 +1,12 @@
+rm platforms/android/project.properties
+android update project --subprojects --path "platforms/android" --target android-19 --library "CordovaLib"
+android update project --subprojects --path "platforms/android" --target android-19 --library "FacebookLib"
 cd platforms/android/
 ant clean
 cd FacebookLib
 ant clean
 ant release
-cd ../../..
-
+cd ../../www
+bower install
+cd ..
 cordova build android
