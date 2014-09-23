2a23ded7f1c2a903840ae4ce5ec34dda742fe157
Diego Sisto <suelopoder@gmail.com>
2014-09-22 22:04:09 -0300

    Mejoré la UI de locations/history para que sea más coherente con el resto

--- www/js/controllers/locations/history_controller.js
+++ www/js/controllers/locations/history_controller.js
@@ -1,13 +1,18 @@
 'use strict';
 
 angular.module('tell.controllers')
-  .controller('LocationsHistoryController', function($scope, $routeParams, historyService, $window) {
+  .controller('LocationsHistoryController', function($scope, $routeParams, historyService, $window, $location) {
     $scope.$on("back_button", function() {
       $window.history.back();
     });
     
     $scope.locations = historyService.get();
+    
     $.each($scope.locations, function(index, location){
       location.when = moment(location.when).fromNow();
     });
+    
+    $scope.go = function(id) {
+      $location.path("/locations/" + id);
+    }
   });
