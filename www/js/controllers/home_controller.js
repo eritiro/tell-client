c58f5d319c125f3ad9c519d68b29f1143d9e2838
eritiro <eritiro@gmail.com>
2014-10-11 21:03:08 -0300

    differences between connection error and scan error

--- www/js/controllers/home_controller.js
+++ www/js/controllers/home_controller.js
@@ -2,7 +2,7 @@
 
 angular.module('tell.controllers')
   .controller('HomeController', function($scope, $location, userSession, Location, scanService, backButtonService) {
-    
+
     $scope.scan = function() {
       scanService.scan( function(scanResult){
         if (scanResult.cancelled){
@@ -12,7 +12,11 @@ angular.module('tell.controllers')
         Location.scan({ url: scanResult.text }, function(location){
           $location.path("/locations/" + location.id);
         }, function(error){
-          alert("El código QR escaneado no es un código válido. Contiene lo siguiente: " + scanResult.text);
+          if(error.status === 0){
+            alert("No se puede conectar con el servidor. Revise su conexión a internet.");
+          } else {
+            alert("El código QR escaneado no es un código válido. Contiene lo siguiente: " + scanResult.text);
+          }
         });
         $scope.$apply();
       }, function(error) {
