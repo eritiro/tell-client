f9ffab8a6a1d0cd1025913567ff9a4964fc6da8c
Diego Sisto <suelopoder@gmail.com>
2014-11-03 21:13:13 -0300

    Hice dinámico el maquetado de los que van al boliche, falta paginado

--- www/js/controllers/users_controller.js
+++ www/js/controllers/users_controller.js
@@ -4,38 +4,15 @@ angular.module('tell.controllers')
   .controller('UsersController', function(Auth, User, $scope, $location, userSession, formHelper, facebookService) {
 
     $scope.user = new formHelper.Model();
-    $scope.user.username = $location.search().guessed_username;
     $scope.user.device_token = registrationId;
 
     function nextStep(user){
       if(!user.username){
-        user.username = user.guessed_username
+        user.username = user.guessed_username;
       }
 
       $location.path("/home");
     }
-
-    $scope.signIn = function() {
-      Auth.login($scope.user).then(function(user) {
-        userSession.storeUser(user);
-        nextStep(user);
-      }, function(response) {
-        $scope.error = "email o password incorrecto.";
-      });
-    };
-
-    $scope.signUp = function() {
-      Auth.register($scope.user).then(function(user) {
-        userSession.storeUser(user);
-        nextStep(user);
-      }, $scope.user.showErrors);
-    };
-
-    $scope.setUsername = function() {
-      User.update({ user: $scope.user }, function() {
-        $location.url("/tutorial/1");
-      }, $scope.user.showErrors);
-    };
 
     $scope.fbLogin = function() {
       facebookService.login(
