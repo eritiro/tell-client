0cc7f73ceafe237f897d1cdaf137c9e11adb1f5f
Diego Sisto <suelopoder@gmail.com>
2014-11-03 21:32:45 -0300

    Agregué un caruusel de bootstrap para los que van al boliche

--- www/js/controllers/locations/show_controller.js
+++ www/js/controllers/locations/show_controller.js
@@ -4,9 +4,13 @@ angular.module('tell.controllers')
   .controller('LocationsShowController', function($scope, $routeParams, Location, historyService) {
     Location.get({ id: $routeParams.id }, function(location) {
       $scope.location = location;
+      $scope.pages = [
+        { attending: location.attending, active: true },
+        { attending: location.attending, active: false },
+        { attending: location.attending, active: false }
+      ]
       historyService.log(location);
     });
 
     $scope.score = 0;
   });
-
