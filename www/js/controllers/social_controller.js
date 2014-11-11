739ef670f4f50bf0df579885367a81a258ca25d2
Diego Sisto <suelopoder@gmail.com>
2014-11-10 21:06:47 -0300

    Implementé en la UI el botón de enviar un trago

--- www/js/controllers/social_controller.js
+++ www/js/controllers/social_controller.js
@@ -1,9 +1,18 @@
 'use strict';
 
-angular.module('tell.controllers').controller('SocialController', function(Auth, User, $scope, $location, userSession, formHelper, facebookService) {
+angular.module('tell.controllers').controller('SocialController', function($scope, $location) {
   $scope.user = {
     id: 4,
     name: "Emiliano Ritiro",
-    photo: "https://scontent-b-mia.xx.fbcdn.net/hphotos-xfp1/v/t1.0-9/10484497_10202900524248031_6895216917458023588_n.jpg?oh=00374ee34a14234ea44d94679fcfd05f&oe=54F64E12"
-  }
+    photo: "https://scontent-b-mia.xx.fbcdn.net/hphotos-xfp1/v/t1.0-9/10484497_10202900524248031_6895216917458023588_n.jpg?oh=00374ee34a14234ea44d94679fcfd05f&oe=54F64E12",
+    invited: false
+  };
+  
+  $scope.invite = function() {
+    $scope.user.invited = true;
+  };
+
+  $scope.chat = function() {
+    $location.path('/users/' + $scope.user.id + '/chat');
+  };
 });
