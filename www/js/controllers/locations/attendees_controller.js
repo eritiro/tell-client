8bbb59de15ff83c13956f58d49fbc690f183d109
eritiro <eritiro@gmail.com>
2014-11-14 10:02:51 -0300

    renamed routes

---
+++ www/js/controllers/locations/attendees_controller.js
@@ -0,0 +1,31 @@
+'use strict';
+
+angular.module('tell.controllers')
+  .controller('LocationsAttendeesController', function($scope, $routeParams, Location, historyService) {
+
+    var pagesConfig = { perPage: 9, maxPages: 10 };
+
+    var paginate = function(items, pagesConfig) {
+      if (!items) return [];
+
+      var pages = Math.min(Math.ceil(items.length / pagesConfig.perPage), pagesConfig.maxPages);
+      var result = [];
+
+      for (var i = 0; i < pages; i++) {
+        var start = i * pagesConfig.perPage;
+        var page = { active:false, items: items.slice(start, start + pagesConfig.perPage) };
+        result.push(page);
+      }
+
+      return result;
+    }
+
+    Location.get({ id: $routeParams.id }, function(location) {
+      $scope.location = location;
+      var items = location.attending;
+      $scope.pages = paginate(items, pagesConfig);
+      historyService.log(location);
+    });
+
+    $scope.score = 0;
+  });
---
+++ www/js/routes.js
@@ -0,0 +1,26 @@
+angular.module('tell').config(function(AuthProvider, $httpProvider, $routeProvider){
+  AuthProvider.loginPath(config.serverUrl + '/users/sign_in');
+  AuthProvider.logoutPath(config.serverUrl + '/users/sign_out');
+  AuthProvider.registerPath(config.serverUrl + '/users');
+  AuthProvider.ignoreAuth(true);
+
+  $httpProvider.defaults.headers.common.Accept = 'application/json';
+
+  $routeProvider.when('/', { template: '<div></div>', controller: 'StartController' });
+
+  $routeProvider.when('/users/sign_up_selection', {templateUrl: 'partials/users/sign_up_selection.html', controller: 'UsersController'});
+  $routeProvider.when('/users/email_sign_in', {templateUrl: 'partials/users/email_sign_in.html', controller: 'UsersController'});
+  $routeProvider.when('/users/email_sign_up', {templateUrl: 'partials/users/email_sign_up.html', controller: 'UsersController'});
+  $routeProvider.when('/users/username', {templateUrl: 'partials/users/username.html', controller: 'UsersController'});
+
+  $routeProvider.when('/users/:id', {templateUrl: 'partials/users/user.html', controller: 'SocialController'});
+
+  $routeProvider.when('/users/:id/chat', {templateUrl: 'partials/users/chat.html', controller: 'ChatController'});
+
+  $routeProvider.when('/locations', {templateUrl: 'partials/locations/find.html', controller: 'LocationsFindController'});
+  $routeProvider.when('/locations/:id', {templateUrl: 'partials/locations/show.html', controller: 'LocationsShowController'});
+  $routeProvider.when('/locations/:id/attendees', {templateUrl: 'partials/locations/attendees.html', controller: 'LocationsAttendeesController'});
+
+  $routeProvider.when('/home', {templateUrl: 'partials/home.html', controller: 'HomeController'});
+  $routeProvider.when('/locations/:locationId/comments/new', { templateUrl: 'partials/comment.html', controller: 'CommentsController' });
+});
