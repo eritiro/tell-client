ebeab5bc5ba068624f991cadea42ad273d79fc2b
Diego Sisto <suelopoder@gmail.com>
2014-11-15 16:57:46 -0300

    Deshardcodee el user en /user/id. Ahora lo trae del server

--- www/js/controllers/social_controller.js
+++ www/js/controllers/social_controller.js
@@ -1,13 +1,11 @@
 'use strict';
 
-angular.module('tell.controllers').controller('SocialController', function($scope, $location) {
-  $scope.user = {
-    id: 4,
-    name: "Emiliano Ritiro",
-    photo: "https://scontent-b-mia.xx.fbcdn.net/hphotos-xfp1/v/t1.0-9/10484497_10202900524248031_6895216917458023588_n.jpg?oh=00374ee34a14234ea44d94679fcfd05f&oe=54F64E12",
-    invited: false
-  };
-  
+angular.module('tell.controllers').controller('SocialController', function($scope, $location, User, $routeParams) {
+
+	User.get({ id: $routeParams.id }, function(user){
+		$scope.user = user;
+	});
+	
   $scope.invite = function() {
     $scope.user.invited = true;
   };
--- www/js/resources/user.js
+++ www/js/resources/user.js
@@ -1,17 +1,20 @@
 'use strict';
 
-angular.module('tell.resources')
-  .factory('User', function($resource){
-    return $resource(
-      config.serverUrl +'/users/:action',
-      { },
-      {
-        update: {
-          method: 'PUT'
-        },
-        facebook: {
-          method: 'PUT',
-          params: { action: 'facebook' }
-        }
-      });
-  });
+angular.module('tell.resources').factory('User', function($resource){
+	return $resource(
+		config.serverUrl +'/users/:id:action',
+		{ },
+		{
+		update: {
+			method: 'PUT'
+		},
+		facebook: {
+			method: 'PUT',
+			params: { action: 'facebook' }
+		},
+		get: { // TODO hace cache de angular
+      cache: true,
+			method: 'GET'
+		}
+		});
+});
