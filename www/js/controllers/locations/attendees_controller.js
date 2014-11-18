04537ef69d24fcce367d5cd4d46b0b168ab3a9b1
eritiro <eritiro@gmail.com>
2014-11-18 16:43:49 -0300

    attendees styles

--- www/js/controllers/locations/attendees_controller.js
+++ www/js/controllers/locations/attendees_controller.js
@@ -3,29 +3,12 @@
 angular.module('tell.controllers')
   .controller('LocationsAttendeesController', function($scope, $routeParams, Location) {
 
-    var pagesConfig = { perPage: 9, maxPages: 10 };
-
-    var paginate = function(items, pagesConfig) {
-      if (!items) return [];
-
-      var pages = Math.min(Math.ceil(items.length / pagesConfig.perPage), pagesConfig.maxPages);
-      var result = [];
-
-      for (var i = 0; i < pages; i++) {
-        var start = i * pagesConfig.perPage;
-        var page = { active:false, items: items.slice(start, start + pagesConfig.perPage) };
-        result.push(page);
-      }
-
-      return result;
-    }
-
 	Location.get({ id: $routeParams.id }, function(location) {
 		$scope.location = location;
 	});
 
     Location.attendees({ id: $routeParams.id }, function(attendees) {
-      $scope.pages = paginate(attendees, pagesConfig);
+      $scope.attendees = attendees;
     });
 
     $scope.score = 0;
