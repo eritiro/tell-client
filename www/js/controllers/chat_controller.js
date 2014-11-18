25f2f07b8666512afcb6f278468eaa25529a7061
eritiro <eritiro@gmail.com>
2014-11-18 01:20:13 -0300

    chat updates via push notifications

--- www/js/controllers/chat_controller.js
+++ www/js/controllers/chat_controller.js
@@ -1,33 +1,40 @@
 'use strict';
 
 angular.module('tell.controllers').controller('ChatController', function($scope, $location, $routeParams, User, userSession, $resource) {
-
   $scope.me = userSession.currentUser();
 
-	var user;
+  var user;
   User.get({ id: $routeParams.id }, function(u) {
-		user = u;
-		$scope.user = user;
+    user = u;
+    $scope.user = user;
   });
-	
-	var Message = $resource(
-		config.serverUrl + '/users/:userId/messages/:messageId',
-		{ userId: $routeParams.id, messageId:'@id' }, 
-		{ }
-	 );
-	$scope.comments = Message.query();
-	
+
+  var Message = $resource(
+    config.serverUrl + '/users/:userId/messages/:messageId',
+    { userId: $routeParams.id, messageId:'@id' },
+    { }
+  );
+
+  $scope.comments = Message.query();
+
   $scope.chat = function() {
     if (!$scope.comment) {
       return;
     }
 
-		$scope.error = false; // TODO error handling
-		var message = new Message({ text: $scope.comment });
-		message.$save();
-		
-		message.mine = true;
-		$scope.comments.push(message);
-		$scope.comment = "";
+    $scope.error = false; // TODO error handling
+    var message = new Message({ text: $scope.comment });
+    message.$save();
+
+    message.mine = true;
+    $scope.comments.push(message);
+    $scope.comment = "";
   };
+
+  $scope.$on('notification',function(event, notification){
+    if($routeParams.id === notification.from_id.toString() && notification.type === "message") {
+      $scope.comments.push({ text: notification.text, mine: false});
+    }
+  });
+
 });
