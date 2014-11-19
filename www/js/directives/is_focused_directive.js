2e45f49548877f996e1f34db25a3d2d473c5a4e9
eritiro <eritiro@gmail.com>
2014-11-19 13:11:24 -0300

    improved chat usability

---
+++ www/js/directives/is_focused_directive.js
@@ -0,0 +1,19 @@
+ angular.module('tell.directives').directive('isFocused', function($timeout) {
+      return {
+        scope: { trigger: '@isFocused' },
+        link: function(scope, element) {
+          scope.$watch('trigger', function(value) {
+            if(value === "true") {
+              $timeout(function() {
+                element[0].focus();
+
+                element.on('blur', function() {
+                  element[0].focus();
+                });
+              });
+            }
+
+          });
+        }
+      };
+    });
