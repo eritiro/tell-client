84d01ac604cd4a839923186885d818e6e62dc535
eritiro <eritiro@gmail.com>
2014-11-23 16:57:11 -0300

    Removed 0 from httperror interceptor

--- www/js/services/httperror_interceptor.js
+++ www/js/services/httperror_interceptor.js
@@ -1,16 +1,14 @@
 'use strict';
 
 angular.module('tell.services').factory('httpErrorInterceptor', function ($q, $location) {
-    return {
-        responseError: function (response) {
-						if (!response.status || response.status === 401) {
-							// asumo error de authenticación
-							$location.path("/users/sign_up_selection");
-						}
-						
-            return $q.reject(response);
-        }
-    };
+  return {
+    responseError: function (response) {
+      if (response.status === 401) {
+        $location.path("/users/sign_up_selection");
+      }
+      return $q.reject(response);
+    }
+  };
 })
 .config(function ($httpProvider) {
     $httpProvider.interceptors.push('httpErrorInterceptor');
