5383651c871107e244c37f0a0978200cb5f99882
eritiro <eritiro@gmail.com>
2014-11-24 13:52:27 -0300

    usability

--- www/js/controllers/locations/show_controller.js
+++ www/js/controllers/locations/show_controller.js
@@ -18,10 +18,23 @@ angular.module('tell.controllers')
       });
     }
 
+    $scope.showAttendees = function(){
+      $location.path("/locations/" + $routeParams.id + "/attendees");
+    };
+
+    $scope.leave = function(){
+      navigator.notification.confirm("Ya no vas a ir a " + $scope.location.name, function(result){
+        if(result === 1){
+          $scope.location.$leave({ id: $routeParams.id }, function(){
+            $rootScope.attendingLocationId = null;
+            $location.path("/home");
+          });
+        }
+      }, "¿Estás seguro?");
+    };
+
     $scope.attend = function() {
-      if ($rootScope.attendingLocationId === parseInt($routeParams.id, 10)){
-        $location.path("/locations/" + $routeParams.id + "/attendees");
-      } else if($rootScope.attendingLocationId) {
+      if($rootScope.attendingLocationId) {
         Location.get({ id: $rootScope.attendingLocationId }, function(oldLocation) {
           navigator.notification.confirm("Ya no vas a ir a " + oldLocation.name, function(result){
           if(result === 1){
