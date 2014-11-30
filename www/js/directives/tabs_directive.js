01a30c176771a9128b7984c444087aa81d80b50a
Diego <suelopoder@gmail.com>
2014-11-30 19:54:52 -0300

    Agregué los tabs en la pantalla principal del boliche

--- www/js/directives/tabs_directive.js
+++ www/js/directives/tabs_directive.js
@@ -2,6 +2,14 @@
 
 angular.module('tell.directives')
 .directive("tellTabs", function ($rootScope, $location, User) {
+
+	var user;
+  User.profile({}, function(userData) {
+    $rootScope.notificationsCount = userData.unread_notifications;
+    $rootScope.attendingLocationId = userData.attending_location_id;
+		user = userData;
+  });
+	
   return {
     templateUrl: 'partials/tabs.html',
     link: function(scope, element, attrs) {
@@ -17,10 +25,7 @@ angular.module('tell.directives')
 				
 				if (action === 'location') {
 					// TODO ver cómo se maneja esto cuando el user no va a ningúna location aun
-					User.profile({}, function(user) {
-						$location.path('/locations/' + user.attending_location_id + '/');
-					});
-					
+					$location.path('/locations/' + user.attending_location_id + '/');
 					return;
 				}
 				
