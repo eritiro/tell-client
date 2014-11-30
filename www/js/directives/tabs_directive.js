753cf6df275ed1100882beafeb921f4fc0f0a894
Diego <suelopoder@gmail.com>
2014-11-30 19:42:39 -0300

    Agregué la navegación desde el tab

--- www/js/directives/tabs_directive.js
+++ www/js/directives/tabs_directive.js
@@ -1,11 +1,36 @@
 "use strict";
 
 angular.module('tell.directives')
-.directive("tellTabs", function ($rootScope, $location) {
+.directive("tellTabs", function ($rootScope, $location, User) {
   return {
     templateUrl: 'partials/tabs.html',
     link: function(scope, element, attrs) {
-      
+			var isSelected = function(action) {
+				return $location.path().indexOf(action) >= 0;
+			};
+			
+      scope.active = isSelected;
+			scope.go = function(action) {
+				if (isSelected(action)) {
+					return;
+				}
+				
+				if (action === 'location') {
+					// TODO ver cómo se maneja esto cuando el user no va a ningúna location aun
+					User.profile({}, function(user) {
+						$location.path('/locations/' + user.attending_location_id + '/');
+					});
+					
+					return;
+				}
+				
+				if (action === 'find') {
+					// TODO desplegar un input medio loco
+					return;
+				}
+				
+				$location.path(action);
+			}
     }
   };
 });
