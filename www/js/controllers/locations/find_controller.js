701147d6f13ca0d5478b27d8250c304ab3964bee
eritiro <eritiro@gmail.com>
2014-11-30 23:54:51 -0300

    Search

--- www/js/controllers/locations/find_controller.js
+++ www/js/controllers/locations/find_controller.js
@@ -1,15 +1,24 @@
 'use strict';
 
-angular.module('tell.controllers').controller('LocationsFindController', function($scope, $routeParams, Location, $location) {
-
-  Location.find({ name: $routeParams.name }, function(locations) {
-    $scope.locations = locations;
-    $scope.searchName = $routeParams.name;
-    $scope.support = config.support;
-  });
+angular.module('tell.controllers').controller('LocationsFindController', function($scope, $routeParams, Location, $location, $window) {
 
   $scope.go = function(id) {
-    $location.url("/locations/" + id);
+    $location.path("/locations/" + id);
   };
+
+  $scope.back = function(){
+    $window.history.back();
+  }
+
+  $scope.search = function(){
+    Location.find({ name: $scope.searchName }, function(locations) {
+      $scope.locations = locations;
+      $location.search("name", $scope.searchName);
+    });
+  }
+
+  $scope.searchName = $routeParams.name;
+  $scope.support = config.support;
+  $scope.search();
 
 });
--- www/js/directives/tabs_directive.js
+++ www/js/directives/tabs_directive.js
@@ -1,53 +1,53 @@
 "use strict";
 
 angular.module('tell.directives')
-.directive("tellTabs", function ($rootScope, $location, User) {
+  .directive("tellTabs", function ($rootScope, $location, User) {
 
-	var user;
-  User.profile({}, function(userData) {
-    $rootScope.notificationsCount = userData.unread_notifications;
-    $rootScope.attendingLocationId = userData.attending_location_id;
-		user = userData;
+    var user;
+    User.profile({}, function(userData) {
+      $rootScope.notificationsCount = userData.unread_notifications;
+      $rootScope.attendingLocationId = userData.attending_location_id;
+      user = userData;
+    });
+
+    return {
+      templateUrl: 'partials/tabs.html',
+      link: function(scope, element, attrs) {
+
+        scope.searching = false;
+
+        scope.search = function(){
+          scope.searching = true;
+        }
+
+        scope.go = function(action) {
+        if (isSelected(action)) {
+          return;
+        }
+
+        if (action === 'location') {
+          // TODO ver cómo se maneja esto cuando el user no va a ningúna location aun
+          $location.path('/locations/' + user.attending_location_id + '/');
+          return;
+        }
+
+        if (action === 'find') {
+          if (scope.status === 'idle') {
+            scope.status = 'search';
+            return;
+          }
+
+          if (scope.query) {
+            $location.path("/locations").search("name", scope.query);
+          } else {
+            scope.status = 'idle';
+          }
+
+          return;
+        }
+
+        $location.path(action);
+        };
+      }
+    };
   });
-	
-  return {
-    templateUrl: 'partials/tabs.html',
-    link: function(scope, element, attrs) {
-			var isSelected = function(action) {
-				return $location.path().indexOf(action) >= 0;
-			};
-	
-			scope.status = 'iddle';
-			
-      scope.active = isSelected;
-			scope.go = function(action) {
-				if (isSelected(action)) {
-					return;
-				}
-				
-				if (action === 'location') {
-					// TODO ver cómo se maneja esto cuando el user no va a ningúna location aun
-					$location.path('/locations/' + user.attending_location_id + '/');
-					return;
-				}
-				
-				if (action === 'find') {
-					if (scope.status === 'iddle') {
-						scope.status = 'search';
-						return;
-					}
-					
-					if (scope.query) {
-						$location.path("/locations").search("name", scope.query);
-					} else {
-						scope.status = 'iddle';
-					}
-					
-					return;
-				}
-				
-				$location.path(action);
-			}
-    }
-  };
-});
