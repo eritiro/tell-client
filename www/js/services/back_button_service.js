6a9d10fb7532e3372321840f4dfc8fb952fdcea2
eritiro <eritiro@gmail.com>
2014-12-01 20:39:00 -0300

    Back button for home

--- www/js/services/back_button_service.js
+++ www/js/services/back_button_service.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('tell.services').service('backButtonService', function($location, $window) {
+angular.module('tell.services').service('backButtonService', function($location, $window, $rootScope) {
   var that = this;
 
   this.cancel = function() {
@@ -14,12 +14,17 @@ angular.module('tell.services').service('backButtonService', function($location,
     }
 
     var currentPath = $location.path();
-    var noReturn = ['/', '/users/sign_up_selection'];
-    if (noReturn.indexOf(currentPath) !== -1) {
-      navigator.app.exitApp();
-    } else {
-      $window.history.back();
-    }
+    var noReturn = ['/', '/users/sign_up_selection', '/feeds'];
+    var gotoHome = ['/locations/attendees', '/notifications'];
+    $rootScope.$apply(function(){
+      if (noReturn.indexOf(currentPath) !== -1) {
+        navigator.app.exitApp();
+      } else if(gotoHome.indexOf(currentPath) !== -1){
+        $location.path('/feeds');
+      } else {
+        $window.history.back();
+      }
+    });
   };
 
   this.attach = function(){
