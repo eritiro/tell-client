adbc43d0d0d70f6e559839c0501ef3973d995503
Diego <suelopoder@gmail.com>
2014-12-02 21:24:11 -0300

    Los feeds ahora se renderizan como HTML

--- www/js/controllers/feed_controller.js
+++ www/js/controllers/feed_controller.js
@@ -1,11 +1,14 @@
 'use strict';
 
-angular.module('tell.controllers').controller('FeedController', function($scope, $location) {
+angular.module('tell.controllers').controller('FeedController', function($scope, $location, $sce) {
   var i;
   $scope.feeds = [];
-  for (i = 0; i < 100; i++) {
-    $scope.feeds.push({ link: "/users/13/chat", content: "<div>texto texto texto " + i + "</div>" });
+  for (i = 0; i < 10; i++) {
+    $scope.feeds.push({ link: "/users/13/chat", content: $sce.trustAsHtml("<div>texto texto texto " + i + "</div>") });
   }
+  
+  var complex = '<ul style="background-color:red;"><li><b>Ahora</b></li><li><i>renderizo</i></li><li style="background-color:green;">el HTML</li></ul>';
+  $scope.feeds.push({ link: "/notifications", content: $sce.trustAsHtml(complex) });
 
   $scope.resolve = function(feed) {
     if (feed.link) {
