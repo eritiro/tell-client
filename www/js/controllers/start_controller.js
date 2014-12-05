fda30e21aa25f00bf60b43965c9546079bfde4ec
eritiro <eritiro@gmail.com>
2014-12-05 01:08:39 -0300

    Start controller

--- www/js/controllers/start_controller.js
+++ www/js/controllers/start_controller.js
@@ -1,25 +1,9 @@
 'use strict';
 
-angular.module('tell.controllers').controller('StartController', function(userSession, $location, User, $rootScope) {
-  var currentUser = userSession.currentUser();
-
-  if (!currentUser) {
+angular.module('tell.controllers').controller('StartController', function(userSession, $location) {
+  if (userSession.currentUser()) {
+    $location.path("/feeds");
+  } else {
     $location.path("/users/sign_up_selection");
-    return;
   }
-
-  var redirect = true;
-
-  $rootScope.$on("dontRedirect", function(){
-    redirect = false;
-  });
-
-  User.profile({}, function(user) {
-    $rootScope.notificationsCount = user.unread_notifications;
-    $rootScope.attendingLocationId = user.attending_location_id;
-    if(redirect) {
-      $location.path("/feeds");
-    }
-  });
-
 });
