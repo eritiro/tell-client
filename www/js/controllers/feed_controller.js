cce9728cf0e34e6e0c121c3426bc43a4d41af90c
eritiro <eritiro@gmail.com>
2014-12-06 23:34:05 -0300

    feeds

--- www/js/controllers/feed_controller.js
+++ www/js/controllers/feed_controller.js
@@ -3,17 +3,17 @@
 angular.module('tell.controllers').controller('FeedController', function($scope, $location, $sce) {
   var i;
   $scope.feeds = [];
-  for (i = 0; i < 10; i++) {
-    $scope.feeds.push({ link: "/users/13/chat", content: $sce.trustAsHtml("<div>texto texto texto " + i + "</div>") });
-  }
-  
-  var complex = '<ul style="background-color:red;"><li><b>Ahora</b></li><li><i>renderizo</i></li><li style="background-color:green;">el HTML</li></ul>';
-  $scope.feeds.push({ link: "/notifications", content: $sce.trustAsHtml(complex) });
+
+//  $scope.feeds.push({ link: "/users/13/chat", content: $sce.trustAsHtml("texto texto texto") });
 
   $scope.resolve = function(feed) {
     if (feed.link) {
       $location.path(feed.link);
     }
+  };
+
+  $scope.find = function(){
+    $location.path("/locations/");
   };
 
   $scope.swipeLeft = function(){
